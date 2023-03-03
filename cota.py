1ed076fb094164174a2bbe8e7ee23037904df615
knabepicer <43427213+knabepicer@users.noreply.github.com>
2023-03-02 20:24:57 -0600

    updates 6

--- cota.py
+++ cota.py
@@ -10,7 +10,7 @@ class Cota(commands.Cog):
 
     cota = discord.SlashCommandGroup("cota", "Get Call of the Armor unit data")
 
-    @cota.slash_command(description = "Get Call of the Armor unit data")
+    @cota.command(description = "Get Call of the Armor unit data")
     async def unit(ctx, name: str):
         stripped_name = re.sub(r'[^a-zA-Z0-9]','', name)
         with open('cota unit.csv', newline='') as csvfile:
