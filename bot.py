533efe2180bac8ac6ce922cad2e9186867c5d12f
knabepicer <43427213+knabepicer@users.noreply.github.com>
2023-03-05 11:33:03 -0600

    update test

--- bot.py
+++ bot.py
@@ -25,17 +25,13 @@ async def help(ctx):
     unitembed.add_field(name='/7s unit [name]', value="Get Seven Siblings unit data", inline=False)
     await ctx.response.send_message(embed=unitembed)
 
-# @cromar.command(guild_ids=test_ids)
-# async def numserver(ctx):
-#     servers = ""
-#     guilds = await bot.fetch_guilds(limit=150).flatten()
-#     for guild in guilds:
-#         servers += guild.name + '\n'
-#     await ctx.response.send_message(servers)
-
 @cromar.command(guild_ids=test_ids)
 async def test(ctx):
-    await ctx.response.send_message("test")
+    servers = ""
+    guilds = await bot.fetch_guilds().flatten()
+    for guild in guilds:
+        servers += guild.name + '\n'
+    await ctx.response.send_message(servers)
 
 bot.load_extension("bob.bob")
 bot.load_extension("cota.cota")
