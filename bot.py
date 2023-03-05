6ee1179d566910429688798a2d18608353e44b60
knabepicer <43427213+knabepicer@users.noreply.github.com>
2023-03-05 10:43:32 -0600

    testing number of servers

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ bot = discord.Bot()
 #bob = bot.create_group("bob", "Get Bells of Byelen data")
 cromar = bot.create_group("cromar", "Get info about Cromar Bot")
 
-current_ids = [1039354532167176303, 828646591471550474, 1030675539314352252]
+test_ids = [1081749141480288256, 1039354532167176303]
 
 @cromar.command(description="Get information about Cromar Bot.") # this decorator makes a slash command
 async def help(ctx): 
@@ -21,18 +21,18 @@ async def help(ctx):
     unitembed.add_field(name='/bob item [name]', value="Get Bells of Byelen item data", inline=False)
     unitembed.add_field(name='/bob skill [name]', value="Get Bells of Byelen skill data", inline=False)
     unitembed.add_field(name='/cota unit [name]', value="Get Call of the Armor unit data", inline=False)
-    unitembed.add_field(name='/c u7sit [name]', value="Get Seven Siblings unit data", inline=False)
+    unitembed.add_field(name='/7s unit [name]', value="Get Seven Siblings unit data", inline=False)
     await ctx.response.send_message(embed=unitembed)
 
-
- #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
+@cromar.command(guild_ids=test_ids)
+async def numservers(ctx):
+    await ctx.response.send_message(discord.Client.guilds)
 
 bot.load_extension("bob.bob")
 bot.load_extension("cota.cota")
 bot.load_extension("7s.sevens")
 @bot.event
 async def on_ready():
-    #await bot.load_extension("cota")
     
     print("Ready!")
 
