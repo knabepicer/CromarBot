faa511e0710a58be0733675634937f49e8768714
knabepicer <43427213+knabepicer@users.noreply.github.com>
2023-03-05 21:28:08 -0600

    tlp additions

--- bot.py
+++ bot.py
@@ -8,8 +8,6 @@ from discord.ext import commands
 
 bot = discord.Bot()
 
-
-#bob = bot.create_group("bob", "Get Bells of Byelen data")
 cromar = bot.create_group("cromar", "Get info about Cromar Bot")
 
 test_ids = [1039354532167176303]
@@ -24,12 +22,14 @@ async def help(ctx):
     unitembed.add_field(name='/cota unit [name]', value="Get Call of the Armor unit data", inline=False)
     unitembed.add_field(name='/7s unit [name]', value="Get Seven Siblings unit data", inline=False)
     unitembed.add_field(name='/trtr unit [name]', value="Get The Road to Ruin unit data", inline=False)
+    unitembed.add_field(name='/tlp unit [name]', value="Get The Last Promise unit data", inline=False)
     await ctx.response.send_message(embed=unitembed)
 
 bot.load_extension("bob.bob")
 bot.load_extension("cota.cota")
 bot.load_extension("7s.sevens")
 bot.load_extension("trtr.trtr")
+bot.load_extension("tlp.tlp")
 @bot.event
 async def on_ready():
     
