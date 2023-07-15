fa09f8f4a91e55bb1bb84ac9f58e23683e8661be
knabepicer <43427213+knabepicer@users.noreply.github.com>
2023-07-15 14:58:58 -0500

    first pass at adding b-sides data

--- burger/burger.py
+++ burger/burger.py
@@ -7,9 +7,11 @@ from discord import option
 
 def get_unit_pages(row):
     unitembed=discord.Embed(title=row['Name'] + " " + row['Affinity'], color=0xd9d021)
-    supportembed=discord.Embed(title=row['Name'] + " " + row['Affinity'], color=0xd9d021)
+    #supportembed=discord.Embed(title=row['Name'] + " " + row['Affinity'], color=0xd9d021)
+    sidesembed=discord.Embed(title=row['Name'] + " " + row['Affinity'], color=0xd9d021)
     unitembed.set_thumbnail(url=row['Portrait'])
-    supportembed.set_thumbnail(url=row['Portrait'])
+    #supportembed.set_thumbnail(url=row['Portrait'])
+    sidesembed.set_thumbnail(url=row['Portrait'])
     unitembed.add_field(name="Lv " + row['Lv'] + " ", value=row['Class'], inline=True)
     unitembed.add_field(name="Leadership: ", value=row['Leadership'], inline=True)
     bases = "HP " + row['HP'] + " | " + "Str " + row['Str'] + " | Mag " + row['Mag'] + " | Skl " + row['Skl'] + " | " + "Spd " + row['Spd'] + " | " + "Lck " + row['Luck'] + " | " + "Def " + row['Def'] + " | " + "Res " + row['Res'] + " | " + "Con " + row['Con'] + " | " + "Mov " + row['Mov']
@@ -31,6 +33,26 @@ def get_unit_pages(row):
     #             if(supportrow['Partner 1'] != 'None'):
     #                 supportstring += supportrow['Partner 1'] + " : Base: " + supportrow['Starting Value 1'] + " | Growth: +" + supportrow['Growth 1'] + "\n"
     #             supportembed.add_field(name="", value=supportstring, inline=False)
+    with open('burger/burger_sides.csv', newline='', encoding="utf-8-sig") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for sidesrow in reader:
+            if(row['Name'] == sidesrow['Name']):
+                if (sidesrow['Special'] == 'Yes'):
+                    sidesembed.add_field(name="", value="Special Class", inline=True)
+                onea += burger_get_sides_info(sidesrow, " 1A")
+                sidesembed.add_field(name="First B-Side", value=onea, inline=True)
+                if (sidesrow['Recursive'] != 'Yes'):
+                    oneb += burger_get_sides_info(sidesrow, " 1B")
+                    sidesembed.add_field(name="Second B-Side", value=oneb, inline=True)
+                    if (sidesrow['Promotion Class'] != ''):
+                        sidesembed.add_field(name='Promoted Class', value=sidesrow['Promotion Class'], inline=True)
+                        twoa += burger_get_sides_info(sidesrow, " 2A")
+                        sidesembed.add_field(name="First Promoted B-Side", value=twoa, inline=True)
+                        twob += burger_get_sides_info(sidesrow, " 2B")
+                        sidesembed.add_field(name="Second Promoted B-Side", value=twob, inline=True)
+                if (sidesrow['Note'] != ""):
+                    sidesembed.set_footer(text=sidesrow['Note'])
+                     
 
 
     page_groups = [
@@ -41,12 +63,12 @@ def get_unit_pages(row):
         use_default_buttons=False,
         default=True,
         ),
-        # pages.PageGroup(
-        # pages=[supportembed],
-        # label="Supports",
-        # description="Support data for the unit",
-        # use_default_buttons=False,
-        # )
+         pages.PageGroup(
+         pages=[sidesembed],
+         label="B-Sides",
+         description="Data for up to the first two B-Sides for unit",
+         use_default_buttons=False,
+         )
     ]
 
     return page_groups
@@ -156,6 +178,60 @@ def burger_get_gains(row):
     if (len(gains2) > 0):
         gains2 = gains2[:-3]
     return gains + gains2
+
+def burger_get_sides_info(row, suffix):
+    gains = ""
+    gains += row['B-Sides Class' + suffix] + "\n"
+    if (row['HP Gains' + suffix] != '0'):
+        gains += "HP: +" + row['HP Gains'] + " | "
+    if (row['Str Gains'+ suffix] != '0'):
+        gains += "Str: +" + row['Str Gains'] + " | "
+    if (row['Mag Gains'+ suffix] != '0'):
+        gains += "Mag: +" + row['Mag Gains'] + " | "
+    if (row['Skl Gains'+ suffix] != '0'):
+        gains += "Skl: +" + row['Skl Gains'] + " | "
+    if (row['Spd Gains'+ suffix] != '0'):
+        gains += "Spd: +" + row['Spd Gains'] + " | "
+    if (row['Def Gains'+ suffix] != '0'):
+        gains += "Def: +" + row['Def Gains'] + " | "
+    if (row['Res Gains'+ suffix] != '0'):
+        gains += "Res: +" + row['Res Gains'] + " | "
+    if (row['Con Gains'+ suffix] != '0'):
+        if (int(row['Con Gains'+ suffix]) > 0):
+            gains += "Con: +" + row['Con Gains'+ suffix] + " | "
+        else:
+            gains += "Con: " + row['Con Gains'+ suffix] + " | "
+    if (row['Mov Gains'+ suffix] != '0'):
+        if (int(row['Mov Gains'+ suffix]) > 0):
+            gains += "Mov: +" + row['Mov Gains'+ suffix] + " | "
+        else:
+            gains += "Mov: " + row['Mov Gains'+ suffix] + " | "
+    gains = gains[:-3]
+    gains += "\n"
+    ranks = ""
+    if (row['New Sword'+ suffix] != 'None'):
+        ranks += "<:RankSword:1083549037585768510>Sword: " + row['New Sword'+ suffix] + " | "
+    if (row['New Lance'+ suffix] != 'None'):
+        ranks += "<:RankLance:1083549035622846474>Lance: " + row['New Lance'+ suffix] + " | "
+    if (row['New Axe'+ suffix] != 'None'):
+        ranks += "<:RankAxe:1083549032292548659>Axe: " + row['New Axe'+ suffix] + " | "
+    if (row['New Bow'+ suffix] != 'None'):
+        ranks += "<:RankBow:1083549033429205073>Bow: " + row['New Bow'+ suffix] + " | "
+    if (row['New Staff'+ suffix] != 'None'):
+        ranks += "<:RankStaff:1083549038936326155>Staff: " + row['New Staff'+ suffix] + " | "
+    if (row['New Anima'+ suffix] != 'None'):
+        ranks += "<:RankAnima:1083549030598049884>Anima: " + row['New Anima'+ suffix] + " | "
+    if (row['New Light'+ suffix] != 'None'):
+        ranks += "<:RankLight:1083549037019541614>Light: " + row['New Light'+ suffix] + " | "
+    if (row['New Dark'+ suffix] != 'None'):
+        ranks += "<:RankDark:1083549034310012959>Dark: " + row['New Dark'+ suffix] + " | "
+    if (len(ranks) > 0):
+        ranks = ranks[:-3]
+    else:
+        ranks = "No Change"
+    gains += ranks
+    return gains
+
 
 def get_unit_names(ctx):
     names = ["BK","Sephiran","Fiona","Hugh","Chungis","Dalvin","Sharlow","Matthis","Soldier","Rebecca","Priscilla","Sail","Perne","Trude","Lynhardt","Salem","Brown","Kaga","Rad Quetz","Tomas","Generic Ilian","ST4F-BOT","Bigle","Deke","Bors","Marty","Azel","Bernie","The King","Papaya","Not Kelik","Honse","Samto","Demijagen","Wario","Ricardo","Dorshua","Subject 35","Joshua","Butter Dog","Riddel","Tailtiu","Kempf","Adachy"]
