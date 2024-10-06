2467ff517bf80200720027764ba690499d93578c
knabepicer <43427213+knabepicer@users.noreply.github.com>
2024-10-05 21:18:14 -0500

    item fix 12

--- cc/cc.py
+++ cc/cc.py
@@ -121,7 +121,7 @@ async def item(ctx, name: str):
         was_found = False
         for row in reader:
             stripped_row = re.sub(r'[^a-zA-Z0-9]','', row['Name'])
-            print(stripped_row)
+            #print(stripped_row)
             if(stripped_row.lower() == stripped_name.lower()):
                 unitembed=discord.Embed(title=row['Name'], color=0x59cad9)
                 #unitembed.set_thumbnail(url=row['Icon'])
@@ -156,8 +156,8 @@ async def item(ctx, name: str):
                 await ctx.response.send_message(embed=unitembed)
                 was_found = True
                 break
-            if (not was_found):
-                await ctx.response.send_message("That item does not exist.")
+        if (not was_found):
+            await ctx.response.send_message("That item does not exist.")
 
 def cc_get_ranks(row):
     ranks = ""
