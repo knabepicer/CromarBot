44e97ba39de5647b0eb7ac689df7f9cf87057e8d
knabepicer <43427213+knabepicer@users.noreply.github.com>
2024-10-05 21:14:35 -0500

    more testing

--- cc/cc.py
+++ cc/cc.py
@@ -116,7 +116,7 @@ async def unit(ctx, name: str):
 
 async def item(ctx, name: str):
     stripped_name = re.sub(r'[^a-zA-Z0-9]','', name)            
-    with open('cc/cc items.csv', newline='') as csvfile:
+    with open('cc/cc items.csv', newline='', encoding="utf-8-sig") as csvfile:
         reader = csv.DictReader(csvfile)
         was_found = False
         for row in reader:
